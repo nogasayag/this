d2dbadbb4cd9fa165ea5c58f446226a81bd28f6a
nogasayag <noga.sayag@gmail.com>
2020-12-30 18:47:55 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+NUM_OF_NUMBERS = 4
+first = int(input("enter the first number:"))
+second = int(input("enter the second number:"))
+third = int(input("enter the third number:"))
+fourth = int(input("enter the fourth number:"))
+sum = first + second + third - fourth
+average = sum / NUM_OF_NUMBERS
+print("the average is: " + str(average))
+
+
