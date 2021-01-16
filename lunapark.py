9b721f1199b8ca9f5a03c2a57dbb91f706d595f9
nogasayag <76785807+nogasayag@users.noreply.github.com>
2021-01-16 20:47:30 +0200

    Create lunapark.py

---
+++ lunapark.py
@@ -0,0 +1,8 @@
+f_name = input("Enter your first name: ")
+l_name = input("Enter your last name: ")
+h = int(input("Enter your height in cm: "))
+age = int(input("Enter your age: "))
+if 12 > age > 6 and 150 > h > 120 :
+    print("you can go on the roller coaster!")
+else:
+    print("you can't go on the roller coaster :( ")
